ee38e337314aa5b27e738eee42829009bbe6c890
naimt20 <nayrobitalavera91@gmail.com>
2026-06-25 20:02:44 +0200

    Refactor DataHandler for Streamlit integration
    Updated DataHandler to use Streamlit secrets for Google Sheets authentication and added error handling.

--- data_manager.py
+++ data_manager.py
@@ -7,23 +7,24 @@ Created on Thu Jun 25 12:14:48 2026
 # -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-
+import streamlit as st
 import gspread
-import os
-import pandas as pd
+import json
 
 class DataHandler:
     def __init__(self, json_filename, spreadsheet_id):
-        base_path = r'C:\proyecto_ganadero'
-        full_path = os.path.join(base_path, json_filename)
-        
-        self.gc = gspread.service_account(filename=full_path)
-        self.sh = self.gc.open_by_key(spreadsheet_id)
-    
-    def get_as_dataframe(self, worksheet_name):
-        worksheet = self.sh.worksheet(worksheet_name)
-        data = worksheet.get_all_records()
-        return pd.DataFrame(data)
+        # Intenta cargar desde los Secrets de Streamlit en la nube
+        try:
+            if "GSPREAD" in st.secrets:
+                creds = json.loads(st.secrets["GSPREAD"]["json"])
+                self.gc = gspread.service_account_from_dict(creds)
+            else:
+                # Si estamos en tu PC local, busca el archivo json
+                self.gc = gspread.service_account(filename=json_filename)
+            self.sh = self.gc.open_by_key(spreadsheet_id)
+        except Exception as e:
+            st.error(f"Error al conectar con Google Sheets: {e}")
 
-    def add_record(self, worksheet_name, list_of_data):
-        worksheet = self.sh.worksheet(worksheet_name)
-        worksheet.append_row(list_of_data)
+    def add_record(self, sheet_name, data):
+        worksheet = self.sh.worksheet(sheet_name)
+        worksheet.append_row(data)
